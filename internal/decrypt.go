fe591d497487982aa54af83790b6d42b51f066dd
Levi Durfee <levi.durfee@gmail.com>
2026-01-19 18:41:06 -0500

    add some checks when decrypting

--- internal/decrypt.go
+++ internal/decrypt.go
@@ -1,8 +1,26 @@
 package internal
 
+import "errors"
+
 // Decrypt recreates the kek from a passphrase and a salt, unwraps the dek using
 // the kek, decrypts the data using the dek, and then returns the plaintext.
 func Decrypt(passphrase string, edek WrappedDEK, ct Ciphertext, salt Salt) ([]byte, error) {
+	if len(passphrase) == 0 {
+		return nil, errors.New("passphrase cannot be empty")
+	}
+
+	if len(edek) == 0 {
+		return nil, errors.New("wrapped DEK cannot be empty")
+	}
+
+	if len(ct) == 0 {
+		return nil, errors.New("ciphertext cannot be empty")
+	}
+
+	if len(salt) == 0 {
+		return nil, errors.New("salt cannot be empty")
+	}
+
 	kek, err := NewKEKFromEnvB64(passphrase, salt)
 	if err != nil {
 		return nil, err
